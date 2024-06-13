697cebe23b1afb4dbd244d2974e8a677cac92a6b
ReginaJM <reg.johansson@gmail.com>
2024-06-13 20:35:45 +0200

    ✨ adds player name form

---
+++ src/components/Players.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+
+
+
+const playerX = ref('');
+const playerO = ref('');
+
+// emit, SKAPA händelser
+const emit = defineEmits<{
+    (e: "sendPlayers", playerX: string, playerO: string): void;
+}>()
+
+const handleSubmit = () => {
+    // console log
+    console.log("submit", playerX.value, playerO.value);
+    
+    // emit, SÄTTA IGÅNG händelsen
+    emit("sendPlayers", playerX.value, playerO.value);
+    playerX.value = "";
+    playerO.value = "";
+}
+
+</script>
+
+
+
+
+
+<template>
+
+    <form @submit.prevent="handleSubmit">
+        <label for="playerX">Player X name:</label><br>
+        <input type="text" id="playerX" v-model="playerX"><br>
+
+        <label for="playerO">Player O name:</label><br>
+        <input type="text" id="playerO" v-model="playerO"><br>
+
+        <input type="submit" value="Start game">
+    </form>
+
+</template>
+
+
+
+
+
+
+
+<style scoped>
+
+</style>
---
+++ src/components/Square.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+
+
+
+</script>
+
+
+
+
+
+<template>
+
+</template>
+
+
+
+
+
+
+
+<style scoped>
+
+</style>
+
+
+
+
+
+
+
